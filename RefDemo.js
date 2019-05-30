2ed1fc5ac7562856e878eb1d73c5fea830c48ee0
dkumar431 <dkumar431@gmail.com>
2019-05-30 07:45:39 +0530

    Refdemo

--- RefDemo.js
+++ RefDemo.js
@@ -1,10 +1,12 @@
+// https://hackernoon.com/refs-in-react-all-you-need-to-know-fb9c9e2aeb81
 import React, {Component} from 'react';
 
 class RefDemo extends Component {
   constructor(){
     super();
     this.state = {
-      name: ""
+      name: "",
+      age: ""
     }
   }
   nameChange = (event) =>{
@@ -15,11 +17,23 @@ class RefDemo extends Component {
       name: this.refs.name.value
     })
   }
+  ageChange = () => {
+    this.setState({
+      age: this.ageNode.value
+    })
+  }
+  setAge = (node) =>{
+    this.ageNode = node;
+  }
   render(){
     return (
       <React.Fragment>
         <input type="text" ref="name" value={this.state.name} onChange={this.nameChange} />
+        <input type="age" ref={this.setAge} value={this.state.age} onChange={this.ageChange} />
+        <br />
         Your name is {this.state.name}
+        <br />
+        Your age is {this.state.age}
       </React.Fragment>
     )
   }
